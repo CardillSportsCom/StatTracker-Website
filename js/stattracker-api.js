477cbfe57d59867b8a0f0a6c2d4784fc3b981332
Jason Rajasegaram <jasonrajasegaram@hotmail.com>
2018-09-27 20:03:37 -0400

    Initial Commit

--- js/stattracker-api.js
+++ js/stattracker-api.js
@@ -1,3 +1,18 @@
 function Initialize(){
-    alert("hey");
+    $.ajax({
+        url: 'http://api-cardillsports-st.herokuapp.com/league',
+        type: 'GET',
+        success: function(data){
+            var HTMLString = "";
+            for(var i = 0; i< data.leagues.length; i ++){
+                HTMLString += "<tr><td>" + data.leagues[i].name +"</td></tr>";
+            }
+            $("#leagueTable").find('tbody').html(HTMLString);
+
+        },
+        error: function(data){
+            alert("couldn't get leagues");
+        }
+
+    })
 }
