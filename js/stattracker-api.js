8b8d750be2359dc6e60d7a5596db8564068dda8c
Jason Rajasegaram <jasonrajasegaram@hotmail.com>
2018-09-27 20:36:16 -0400

    Initial Commit

--- js/stattracker-api.js
+++ js/stattracker-api.js
@@ -6,14 +6,19 @@ function Initialize(){
         success: function(data){
             var HTMLString = "";
             for(var i = 0; i< data.leagues.length; i ++){
-                HTMLString += "<tr><td>" + data.leagues[i].name +" <input type='hidden' class='leagueId' value='" + data.leagues[i]._id +"'/></td></tr>";
+                HTMLString += "<tr><td><span class='leagueName'>" + data.leagues[i].name +"</span><input type='hidden' class='leagueId' value='" + data.leagues[i]._id +"'/></td></tr>";
             }
             $("#leagueTable").find('tbody').html(HTMLString);
-
+            $(".leagueName").on("click", function(){
+                GetLeagueScores($(this).sibling('.leagueId').val());
+            });
         },
         error: function(xhr, status, error) {
             alert(error);
         }
 
     })
+}
+function GetLeagueScores(leagueId){
+    alert(leagueId);
 }
