917e6ab93a6f536ee56f86dadc4ce8ff7533642d
Jason Rajasegaram <jasonrajasegaram@hotmail.com>
2018-12-13 03:52:01 -0500

    login

--- js/stattracker-api.js
+++ js/stattracker-api.js
@@ -41,8 +41,8 @@ ui.start('#firebaseui-auth-container', {
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //   firebase.auth.EmailAuthProvider.PROVIDER_ID,
 
     ],
     // Terms of service url.
